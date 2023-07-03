3e8160a2263bb07018dc66c807fbbd37d1bee464
Arka Chakraborty <arkac720@gmail.com>
2023-07-03 23:40:59 +0530

    structure fix

--- controller/candy.js
+++ controller/candy.js
@@ -3,21 +3,14 @@ const user = require("../model/candyModel");
 exports.getProducts = async function (req, res, next) {
   try {
     let arr = await user.findAll();
+    // console.log(arr,'hi')
     res.status(200).json(arr);
   } catch (error) {
     console.log(error);
   }
 };
-exports.patchProduct = async function (req, res, next) {
-  try {
-    const id = req.params.id;
-    // console.log(req.body)
-    await user.update(req.body, { where: { id: id } });
-    res.send("updated");
-  } catch (error) {
-    console.log(error);
-  }
-};
+
+
 exports.postProduct = async function (req, res, next) {
   try {
     user.create(req.body);
@@ -28,6 +21,19 @@ exports.postProduct = async function (req, res, next) {
     res.send(error);
   }
 };
+
+
+exports.patchProduct = async function (req, res, next) {
+  try {
+    const id = req.params.id;
+    // console.log(req.body)
+    await user.update(req.body, { where: { id: id } });
+    res.send("updated");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
 exports.deleteProduct = async function (req, res, next) {
   try {
